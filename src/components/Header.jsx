930af9e3b571538dbb2c37cc0a794dd5642337f9
Vaibhav Kumar Chauhan <vaibhavchauhan17>
2024-01-15 18:14:00 +0530

    basic app ready

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import QuizLogo from '../assets/quiz-logo.png'
+
+export default function Header(){
+    return <header>
+        <img src={QuizLogo} alt='Quiz Logo'/>
+        <h1>ReactQuiz</h1>
+    </header>
+}
